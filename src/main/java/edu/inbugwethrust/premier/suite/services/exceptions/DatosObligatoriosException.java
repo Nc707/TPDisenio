9a089caff83dec1782ee9d8909c87221f1704ab6
Nicolás Courtalón <courtalonnicolasm@gmail.com>
2025-11-08 14:51:49 -0300

    refactor:Agrega serialVersionUID a excepciones personalizadas del proyecto
    - Añade serialVersionUID a DatosObligatoriosException
    - Añade serialVersionUID a HuespedDuplicadoException

--- src/main/java/edu/inbugwethrust/premier/suite/services/exceptions/DatosObligatoriosException.java
+++ src/main/java/edu/inbugwethrust/premier/suite/services/exceptions/DatosObligatoriosException.java
@@ -3,7 +3,8 @@ package edu.inbugwethrust.premier.suite.services.exceptions;
 import java.util.List;
 
 public class DatosObligatoriosException extends RuntimeException {
-
+	
+	private static final long serialVersionUID = 1L;
     private final List<String> errores;
 
     public DatosObligatoriosException(List<String> errores) {
--- src/main/java/edu/inbugwethrust/premier/suite/services/exceptions/HuespedDuplicadoException.java
+++ src/main/java/edu/inbugwethrust/premier/suite/services/exceptions/HuespedDuplicadoException.java
@@ -1,7 +1,9 @@
 package edu.inbugwethrust.premier.suite.services.exceptions;
 
 public class HuespedDuplicadoException extends RuntimeException {
-
+	
+	private static final long serialVersionUID = 1L;
+	
     public HuespedDuplicadoException(String message) {
         super(message);
     }
