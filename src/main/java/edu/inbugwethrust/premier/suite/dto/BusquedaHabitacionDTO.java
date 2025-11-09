66d33e666da6c62429ff2424bd9010ce05a6a9c6
Nicolás Courtalón <courtalonnicolasm@gmail.com>
2025-11-09 17:48:38 -0300

    Agrega validación @NotNull a fechas en BusquedaHabitacionDTO
    - Añade @NotNull a fechaInicio con mensaje personalizado
    - Añade @NotNull a fechaFin con mensaje personalizado

--- src/main/java/edu/inbugwethrust/premier/suite/dto/BusquedaHabitacionDTO.java
+++ src/main/java/edu/inbugwethrust/premier/suite/dto/BusquedaHabitacionDTO.java
@@ -2,6 +2,7 @@ package edu.inbugwethrust.premier.suite.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BusquedaHabitacionDTO {
+	@NotNull(message = "La fecha de inicio no puede estar vacía")
 	LocalDate fechaInicio;
+	@NotNull(message = "La fecha de fin no puede estar vacía")
 	LocalDate fechaFin;
 }
