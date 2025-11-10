2b9e7b5e02ee70f05dfe4326ecad8aac682907df
Nicolás Courtalón <courtalonnicolasm@gmail.com>
2025-11-10 15:31:40 -0300

    refactor: Replaced mappers by new ones

--- src/main/java/edu/inbugwethrust/premier/suite/mappers/DireccionMapper.java
+++ src/main/java/edu/inbugwethrust/premier/suite/mappers/DireccionMapper.java
@@ -1,46 +1,15 @@
 package edu.inbugwethrust.premier.suite.mappers;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
 import edu.inbugwethrust.premier.suite.dto.DireccionDTO;
 import edu.inbugwethrust.premier.suite.model.Direccion;
 
-@Component
-public class DireccionMapper implements IDirecionMapper {
+@Mapper(componentModel = "spring")
+public interface DireccionMapper{
 
-	@Override
-	public DireccionDTO toDTO(Direccion entity) {
-		if(entity == null) {
-			return null;
-		}
-		DireccionDTO dto = new DireccionDTO();
-		dto.setCalle(entity.getCalle());
-		dto.setNumero(entity.getNumero());
-		dto.setPiso(entity.getPiso());
-		dto.setDepartamento(entity.getDepartamento());
-		dto.setCodigoPostal(entity.getCodigoPostal());
-		dto.setLocalidad(entity.getLocalidad());
-		dto.setProvincia(entity.getProvincia());
-		dto.setPais(entity.getPais());
-		return dto;
-		
-	}
-
-	@Override
-	public Direccion toEntity(DireccionDTO dto) {
-		if(dto == null) {
-			return null;
-		}
-		Direccion entity = new Direccion();
-		entity.setCalle(dto.getCalle());
-		entity.setNumero(dto.getNumero());
-		entity.setPiso(dto.getPiso());
-		entity.setDepartamento(dto.getDepartamento());
-		entity.setCodigoPostal(dto.getCodigoPostal());
-		entity.setLocalidad(dto.getLocalidad());
-		entity.setProvincia(dto.getProvincia());
-		entity.setPais(dto.getPais());
-		return entity;
-	}
+	
+	public DireccionDTO toDTO(Direccion entity);
+	public Direccion toEntity(DireccionDTO dto);
 
 }
--- src/main/java/edu/inbugwethrust/premier/suite/mappers/IMapper.java
+++
@@ -1,8 +0,0 @@
-package edu.inbugwethrust.premier.suite.mappers;
-
-public interface IMapper<D, E> {
-	D toDTO(E entity);
-	
-	E toEntity(D dto);
-
-}
